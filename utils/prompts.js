5d183e4ec98a1753eb047cf0eb04dbac92e62fbb
Marta <maeldeis@gmail.com>
2024-02-23 20:21:14 +0100

    Reworking the prompts

--- utils/prompts.js
+++ utils/prompts.js
@@ -1,26 +1,25 @@
 const inquirer = require("inquirer");
-
 const promptManager = () => {
   return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "What is the team manager's name?",
+      message: "Enter team manager's name",
     },
     {
       type: "input",
       name: "id",
-      message: "What is the team manager's id?",
+      message: "Enter the team manager's id",
     },
     {
       type: "input",
       name: "email",
-      message: "What is the team manager's email?",
+      message: "Enter the team manager's email",
     },
     {
       type: "input",
       name: "officeNumber",
-      message: "What is the team manager's office number?",
+      message: "Enter the team manager's office number",
     },
   ]);
 };
@@ -30,22 +29,22 @@ const promptEngineer = () => {
     {
       type: "input",
       name: "name",
-      message: "What is the engineer's name?",
+      message: "Enter the engineer's name",
     },
     {
       type: "input",
       name: "id",
-      message: "What is the engineer's id?",
+      message: "Enter the engineer's id",
     },
     {
       type: "input",
       name: "email",
-      message: "What is the engineer's email?",
+      message: "Enter the engineer's email",
     },
     {
       type: "input",
       name: "github",
-      message: "What is the engineer's github?",
+      message: "Enter the engineer's github",
     },
   ]);
 };
@@ -55,22 +54,22 @@ const promptInern = () => {
     {
       type: "input",
       name: "name",
-      message: "What is the intern's name?",
+      message: "Enter the intern's name",
     },
     {
       type: "input",
       name: "id",
-      message: "What is the intern's id?",
+      message: "Enter the intern's id",
     },
     {
       type: "input",
       name: "email",
-      message: "What is the intern's email?",
+      message: "Enter the intern's email",
     },
     {
       type: "input",
       name: "school",
-      message: "What is the intern's school?",
+      message: "Enter the intern's school",
     },
   ]);
 };
@@ -80,7 +79,7 @@ const promptAddTeamMembers = () => {
     {
       type: "list",
       name: "addTeamMembers",
-      message: "Select type of team member to add",
+      message: "Select team member to add",
       choices: ["Engineer", "Intern", "None"],
     }
   ]);
