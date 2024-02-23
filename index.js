0ac1ec632d34c5b4578b5c68231b96e4b6792e14
Marta <maeldeis@gmail.com>
2024-02-23 19:54:37 +0100

    Running into bugs recalibrating index

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-const fs = require("fs");
-const path = require("path");
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const {
-    promptManager,
-    promptAddTeamMembers,
-    promptEngineer,
-    promptInern } = require("./utils/prompts.js");
-  const render = require("./src/page-template.js");
-  const teamMembers = [];
+
   const init = async () => {
     try {
         const managerData = await promptManager();
