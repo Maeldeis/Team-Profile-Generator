78e40ebae7f9343824acf4d11343585950a66800
Marta <maeldeis@gmail.com>
2024-02-23 19:56:11 +0100

    changing the git ignore requirements

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-
+const fs = require("fs");
+const path = require("path");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const {
+  promptManager,
+  promptAddTeamMembers,
+  promptEngineer,
+  promptInern } = require("./utils/prompts.js");
+const render = require("./src/page-template.js");
+const OUTPUT_DIR = path.resolve(__dirname, "./output/");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const teamMembers = [];
   const init = async () => {
     try {
         const managerData = await promptManager();
