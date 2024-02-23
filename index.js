e5dbb9f78804ecaf1603aec6d7ca06fde75e5ddb
Marta <maeldeis@gmail.com>
2024-02-23 19:39:02 +0100

    creating loop asking user to add a team member

--- index.js
+++ index.js
@@ -13,5 +13,18 @@ const {
     promptInern } = require("./utils/prompts.js");
   const render = require("./src/page-template.js");
   const teamMembers = [];
-
+  const init = async () => {
+    try {
+        const managerData = await promptManager();
+        const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
+        teamMembers.push(manager);
+        let addMoreTeamMembers = true;
+        while (addMoreTeamMembers) {
+          const { addTeamMembers } = await promptAddTeamMembers();
+          if (addTeamMembers === "Engineer") {
+            const engineerData = await promptEngineer();
+            const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github);
+            teamMembers.push(engineer);
+    
+    
 
