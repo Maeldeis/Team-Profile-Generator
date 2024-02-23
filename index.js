02943591caf28585c9c6cfeac4c5d0761448f487
Marta <maeldeis@gmail.com>
2024-02-23 19:35:04 +0100

    adding the class and utils consts

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const fs = require("fs");
+const path = require("path");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+const {
+    promptManager,
+    promptAddTeamMembers,
+    promptEngineer,
+    promptInern } = require("./utils/prompts.js");
+  const render = require("./src/page-template.js");
+  const teamMembers = [];
 
-const render = require("./src/page-template.js");
-
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 
